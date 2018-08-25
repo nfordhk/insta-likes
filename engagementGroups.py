2ed990b091da53eab6e7edc90834b0e3abde3a23
nfordhk <nick@nickford.me>
2018-08-25 11:33:14 -0700

    Fixed For Loop

--- engagementGroups.py
+++ engagementGroups.py
@@ -36,14 +36,13 @@ with open("autoRounds.txt") as file:
 
 for url in urls:                                                               
     driver.get(url)
+    # Click on First Photo
+    firstPhoto_element = driver.find_elemenet_by_css_selector(' ')
+    firstPhoto_element.click()
 
-# Click on First Photo
-firstPhoto_element = driver.find_elemenet_by_css_selector(' ')
-firstPhoto_element.click()
-
-# Like First Photo
-like_element = driver.find_element_by_css_selector('.coreSpriteHeartOpen')
-like_element.click()
+    # Like First Photo
+    like_element = driver.find_element_by_css_selector('.coreSpriteHeartOpen')
+    like_element.click()
 
 # Exit Browser when finished
 browser.close()
